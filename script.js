9da2c6316bac43c079ec21302ef0796a2614d668
AlejandroRod1992 <rodr0262@algonquinlive.com>
2023-11-26 01:51:50 -0500

    completes assigment

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const emoji = [ ]
-
-for (let i = 128506 ; i <= 128591; i++){
-emoji.push(/html/ <p class=emoji style="text-align: center;"><span 
-style="font-size: 5rem;">&#${i};</span><br><code>${i}</code></p>)
-}
-
-for (let i = 128640 ; i <= 128653; i++){
-emoji.push(/html/ <p class=emoji style="text-align: center;"><span 
-style="font-size: 5rem;">&#${i};</span><br><code>${i}</code></p>)
-}
-
-const $body = document.getElementById('body')
-
-$body.innerHTML += emoji.join('')
+const emojis = [
+    "&#x1F604;", "&#x1F60A;", "&#x1F609;", "&#x1F60D;",
+    "&#x1F618;", "&#x1F917;", "&#x1F525;", "&#x1F60E;",
+    "&#x1F44D;", "&#x1F4A9;", "&#x1F436;", "&#x1F525;"
+  ];
+  
+  const emojiGallery = document.getElementById("emoji-gallery");
+  
+  emojis.forEach((emoji, index) => {
+    const emojiCard = document.createElement("div");
+    emojiCard.classList.add("emoji-card");
+  
+    const emojiSymbol = document.createElement("div");
+    emojiSymbol.innerHTML = emoji;
+    emojiCard.appendChild(emojiSymbol);
+  
+    const emojiCode = document.createElement("div");
+    emojiCode.classList.add("emoji-code");
+    emojiCode.textContent = `Code: ${emoji}`;
+    emojiCard.appendChild(emojiCode);
+  
+    emojiGallery.appendChild(emojiCard);
+  });
+  
